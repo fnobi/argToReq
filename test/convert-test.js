165b88864cb1c84b88381efa67bf72491318052d
fnobi <shin@fnobi.com>
2012-11-23 02:05:56 +0900

    だいたい、欲しい機能は全部書いた。実装はまた今度。

--- test/convert-test.js
+++ test/convert-test.js
@@ -1,6 +1,8 @@
 var buster   = require('buster'),
     argToReq = require(__dirname + '/../');
 
+// ArgToReqにするのも手かも。けっこう設定多い。
+
 buster.testCase('argToReq convert', {
 	'single arg': function () {
 		assert.equals(
@@ -14,6 +16,30 @@ buster.testCase('argToReq convert', {
 			argToReq(['hoge', 'moge', 'fuga']).path,
 			'/hoge/moge/fuga'
 		);
+	},
+
+	'arg with flag': function () {
+		argToReq.flags = ['moge'];
+
+		var req = argToReq(['hoge', '--moge', 'fuga']);
+		assert.equals(req.path, '/hoge/fuga');
+		assert(req.params.moge);
+	},
+
+	'arg with option': function () {
+		argToReq.options = ['moge'];
+
+		var req = argToReq(['hoge', '--moge', 'fuga']);
+		assert.equals(req.path, '/hoge');
+		assert.equals(req.params.moge, 'fuga');
+	},
+
+	'arg with method': function () {
+		argToReq.methodFlags = { 'POST': ['p', 'post'] };
+
+		var req = argToReq(['hoge', '--post', 'fuga']);
+		assert.equals(req.path, '/hoge/fuga');
+		assert.equals(req.method, 'POST');
 	}
 
 });
